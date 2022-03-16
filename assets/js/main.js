f399bca37f178077a435a000c4f5e554e3a5aec6
Constantin Nita <nita.constantin880722@yahoo.com>
2022-03-16 18:32:10 +0200

    despre

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,34 @@
-$('.owl-carousel').owlCarousel({
-    margin: 10,
-    nav: true,
-    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
-    responsive: {
-        0: {
-            items: 1
+$(document).ready(function(){
+    $('#slider-home').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: false,
+    responsive:{
+        0:{
+            items:1
         },
-        600: {
-            items: 3
+        600:{
+            items:1
         },
-        1000: {
-            items: 3
+        1000:{
+            items:1
         }
     }
+    })
+    });
+
+    /* close menu resp */
+    $(".close-navbar").click(function(){
+        $('.navbar-collapse').removeClass('show')
+    });
+
+    /* sticky menu */
+    
+    $(window).scroll(function () {
+        if($(window).scrollTop() > 140 && $(window).width() > 991 ) {
+            $("header").addClass('sticky');
+        } else {
+            $("header").removeClass('sticky');
+        }
 });
